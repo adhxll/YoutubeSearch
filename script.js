01595e97c48ce7dccb1f7adf35dfcbad8553a9f9
Adhella <adhellasbl@gmail.com>
2022-02-09 23:35:41 +0700

    Starter

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("searchQuery").addEventListener("keyup", event => {
+    if(event.key !== "Enter") return; // Use `.key` instead.
+    document.querySelector("searchButton").click(); // Things you want to do.
+    event.preventDefault(); // No need to `return false;`.
+});
+
+function searchYoutube()
+{
+    var elem=document.getElementById('searchQuery');
+    var url="https://www.youtube.com/results?search_query="+encodeURIComponent(elem.value);
+    var win=window.open(url, '_blank');
+    win.focus();
+}
+
+
+
